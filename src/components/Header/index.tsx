793cb428deb9e4fabee2120ef04aae4bbc99e88a
Vaibhav Singh <vaibhav.s@browserstack.com>
2022-11-30 00:49:32 +0530

    refactor(Header): component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
-import { HEADER_HEIGHT } from '../../constants';
 import { NightwatchIcon } from '../../icons';
+import { Center, Date, Left, Right, Time, Wrapper } from './style';
 
 const Header = () => {
   return (
@@ -17,49 +16,5 @@ const Header = () => {
     </Wrapper>
   );
 };
-
-const Wrapper = styled.header`
-  height: ${HEADER_HEIGHT}px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 18px 20px;
-  background: var(--color-dark-background);
-  color: var(--color-dark-background-text);
-`;
-
-const Left = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: baseline;
-`;
-
-const Center = styled.div`
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  font-weight: var(--font-weight-bold);
-  font-size: calc(24 / 16 * 1rem);
-  line-height: var(--line-height-36);
-`;
-
-const Right = styled.div`
-  flex: 1;
-  display: flex;
-  align-items: flex-end;
-  flex-direction: column;
-  font-weight: var(--font-weight-semi-light);
-`;
-
-const Date = styled.div`
-  font-size: 1rem;
-  line-height: var(--line-height-24);
-`;
-
-const Time = styled.div`
-  font-size: calc(12 / 16 * 1rem);
-  line-height: var(--line-height-20);
-  opacity: 0.75;
-`;
 
 export default React.memo(Header);
