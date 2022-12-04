fec65fd7356642dec6693da4e96e22cfae30d44c
Vaibhav Singh <vaibhav.s@browserstack.com>
2022-12-04 20:04:58 +0530

    fix: environment dropdown selection

--- src/components/EnvironmentDropdown/index.tsx
+++ src/components/EnvironmentDropdown/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import {
   DropdownMenuContent,
@@ -12,31 +12,41 @@ import { MetaDataProps } from '../EnvironmentMetadata';
 import EnvironmentContent from './EnvironmentContent';
 import Search from '../Search';
 
+type EnvironmentData = {
+  name: string;
+  meta: MetaDataProps['meta'];
+  testResult: {
+    passed: number;
+    failed: number;
+    skipped: number;
+  };
+};
+
 type EnvironmentDropdownProps = {
-  data: {
-    name: string;
-    meta: MetaDataProps['meta'];
-    testResult: {
-      passed: number;
-      failed: number;
-      skipped: number;
-    };
-  }[];
+  data: EnvironmentData[];
 };
 
 const EnvironmentDropdown: React.FC<EnvironmentDropdownProps> = ({ data }) => {
   const [isDropDownOpen, setDropdownOpen] = useState(false);
   const [context, setContext] = useState('Environment1');
+  const [envData, setEnvData] = useState<Partial<EnvironmentData>>({});
+
+  useEffect(() => {
+    data.find((data) => {
+      if (data.name == context) {
+        setEnvData(data);
+      }
+    });
+  }, [context]);
 
   return (
     <DropdownMenu.Root onOpenChange={(open) => setDropdownOpen(open)}>
       <DropdownMenuTrigger asChild>
         <EnvironmentSelectorWrapper>
-          {/* TODO: Filter using context. Sample value of context is Environment1, Environment2 */}
           <EnvironmentContent
-            name={data[0]['name']}
-            meta={data[0]['meta']}
-            testResult={data[0]['testResult']}
+            name={envData.name || data[0]['name']}
+            meta={envData.meta || data[0]['meta']}
+            testResult={envData.testResult || data[0]['testResult']}
           />
           {isDropDownOpen ? <ArrowDropUp /> : <ArrowDropDown />}
         </EnvironmentSelectorWrapper>
