e21be8724e55c618cc44d1efdab84c97491f83b8
Prudhvi <prudhvi@browserstack.com>
2022-12-06 15:51:45 +0530

    multiple builds with vite

---
+++ vite.config.vrt.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-styled-components']]
+      }
+    }),
+    viteSingleFile()
+  ],
+  base: "",
+  build: {
+    rollupOptions: {
+      input: {
+        vrt: resolve(__dirname, 'index_vrt.html'),
+      },
+      output: {
+          name: "vrt",
+          dir: "dist_vrt",
+        }
+    },
+  },
+});
