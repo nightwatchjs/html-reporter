cfe8b2f62529a4aaa2eba64e221085b184e5048e
Vaibhav Singh <vaibhav.s@browserstack.com>
2023-04-03 15:39:42 +0530

    update: example test

---
+++ nightwatch/nightwatch.ts
@@ -0,0 +1,18 @@
+describe('Nightwatchjs.org Demo', function () {
+  this.tags = ['demo'];
+
+  before((browser) => browser.navigateTo('https://nightwatchjs.org/'));
+
+  it('Demo test nightwatchjs.org', function (browser) {
+    browser
+      .waitForElementVisible('body')
+      .assert.titleContains('Nightwatch.js')
+      .assert.visible('.brand-message')
+      .assert.visible('.button-default.copy.btn-copy')
+      .click('.button-default.copy.btn-copy')
+      .assert.textContains('.npm-install', 'Code copied');
+  });
+
+  after((browser) => browser.end());
+});
+//
